e257c91a1488259e2e1d4917d297dbb2df9e58d0
maythasit thamronglearkait <maythasit.th@gmail.com>
2022-11-14 01:14:11 +0700

    refactor : fomation output and UpperCase

--- coinbase.js
+++ coinbase.js
@@ -2,37 +2,36 @@
 const axios = require('axios');
 
 const getExchangeTrades = async (baseCurrency, quoteCurrency) => {
-    const url = `https://api.exchange.coinbase.com/products/${baseCurrency}-${quoteCurrency}/trades?limit=1`;
+    const url = `https://api.exchange.coinbase.com/products/${baseCurrency.toUpperCase()}-${quoteCurrency.toUpperCase()}/trades?limit=1`;
     const response = await axios.get(url);
     const { data } = response;
     
-    
     return data.map(item => ({
-        base_currency: baseCurrency,
-        quote_currency: quoteCurrency,
-        base_volume: undefined,
-        quote_volume: undefined,
+        base_currency: baseCurrency.toUpperCase(),
+        quote_currency: quoteCurrency.toUpperCase(),
+        base_volume: item.size,
+        quote_volume: item.size * item.price,
         side: item.side,
         time: item.time,  
     }));
 };
 
 const getExchangeVolume = async (baseCurrency, quoteCurrency) => {
-    const url = `https://api.exchange.coinbase.com/products/${baseCurrency}-${quoteCurrency}/stats`;
+    const url = `https://api.exchange.coinbase.com/products/${baseCurrency.toUpperCase()}-${quoteCurrency.toUpperCase()}/stats`;
     const response = await axios.get(url);
     const { data } = response;
     
     return {
-        base_currency: baseCurrency,
-        quote_currency: quoteCurrency,  
+        base_currency: baseCurrency.toUpperCase(),
+        quote_currency: quoteCurrency.toUpperCase(),  
         base_volume: data.volume,
-        quote_volume: undefined,
+        quote_volume: data.volume_30day,
        
     };
 };
 
 const getLastRate = async (baseCurrency, quoteCurrency) => {
-    const url = `https://api.exchange.coinbase.com/products/${baseCurrency}-${quoteCurrency}/ticker`;
+    const url = `https://api.exchange.coinbase.com/products/${baseCurrency.toUpperCase()}-${quoteCurrency.toUpperCase()}/ticker`;
     const response = await axios.get(url);
     const { data } = response;
     
@@ -40,14 +39,18 @@ const getLastRate = async (baseCurrency, quoteCurrency) => {
 };
 
 const getOrderBook = async (baseCurrency, quoteCurrency) => {
-    const url = `https://api.exchange.coinbase.com/products/${baseCurrency}-${quoteCurrency}/book?level=3`;
+    const url = `https://api.exchange.coinbase.com/products/${baseCurrency.toUpperCase()}-${quoteCurrency.toUpperCase()}/book?level=2`;
     const response = await axios.get(url);
     const { data } = response;
-    
+    const addPropertyNameBids = data.bids.map((row) =>{
+        return { price : row[0], quantity : row[1] }
+    });
+    const addPropertyNameAsks = data.asks.map((row) =>{
+        return { price : row[0], quantity : row[1] }
+    });
+
     return {
-        bids: data.bids,
-        asks: data.asks,
+        bids: addPropertyNameBids,
+        asks: addPropertyNameAsks,
     };
 };
-
-
