3400396eaa2e46a4ad9d8125c57ee55e2ccc3bc0
maythasit thamronglearkait <maythasit.th@gmail.com>
2022-11-13 18:08:11 +0700

    refactor: format data

--- coinbase.js
+++ coinbase.js
@@ -5,23 +5,20 @@ const getExchangeTrades = async (baseCurrency, quoteCurrency) => {
     const url = `https://api.exchange.coinbase.com/products/${baseCurrency}-${quoteCurrency}/trades?limit=1`;
     const response = await axios.get(url);
     const { data } = response;
-   
+    
+    
     return data.map(item => ({
         base_currency: baseCurrency,
         quote_currency: quoteCurrency,
         base_volume: undefined,
         quote_volume: undefined,
         side: item.side,
-        time: item.time,
-        size: item.size,
-        trade_id: item.trade_id,
-        price: item.price,
-        
+        time: item.time,  
     }));
 };
 
 const getExchangeVolume = async (baseCurrency, quoteCurrency) => {
-    const url = `https://api.pro.coinbase.com/products/${baseCurrency}-${quoteCurrency}/stats`;
+    const url = `https://api.exchange.coinbase.com/products/${baseCurrency}-${quoteCurrency}/stats`;
     const response = await axios.get(url);
     const { data } = response;
     
@@ -29,14 +26,13 @@ const getExchangeVolume = async (baseCurrency, quoteCurrency) => {
         base_currency: baseCurrency,
         quote_currency: quoteCurrency,  
         base_volume: data.volume,
-        open: data.open,
-        high: data.high,
-        low: data.low,
+        quote_volume: undefined,
+       
     };
 };
 
 const getLastRate = async (baseCurrency, quoteCurrency) => {
-    const url = `https://api.pro.coinbase.com/products/${baseCurrency}-${quoteCurrency}/ticker`;
+    const url = `https://api.exchange.coinbase.com/products/${baseCurrency}-${quoteCurrency}/ticker`;
     const response = await axios.get(url);
     const { data } = response;
     
@@ -44,7 +40,7 @@ const getLastRate = async (baseCurrency, quoteCurrency) => {
 };
 
 const getOrderBook = async (baseCurrency, quoteCurrency) => {
-    const url = `https://api.pro.coinbase.com/products/${baseCurrency}-${quoteCurrency}/book?level=3`;
+    const url = `https://api.exchange.coinbase.com/products/${baseCurrency}-${quoteCurrency}/book?level=3`;
     const response = await axios.get(url);
     const { data } = response;
     
@@ -54,5 +50,4 @@ const getOrderBook = async (baseCurrency, quoteCurrency) => {
     };
 };
 
-getOrderBook('ETH','BTC').then((result) =>{ console.log(result)
-});
+
