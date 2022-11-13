f6e414078eca0405a6705bb1d0ec60101ddf3354
maythasit thamronglearkait <maythasit.th@gmail.com>
2022-11-13 17:01:09 +0700

    refactor

--- coinbase.js
+++ coinbase.js
@@ -1,14 +1,6 @@
 // this will be where all the funtion would be
 const axios = require('axios');
-/*
-Create a “getExchangeTrades” function to retrieve the latest exchange trades of specific
-trading pairs. The function should have input parameter “baseCurrency” and
-“quoteCurrency” and return output format as the example object;
-The baseCurrency and QuoteCurrency input can be capital or lowercase but the output result
-the format should always show base_currency and quote_currency in capital letters.
-The example shows the result from input baseCurrency in as “ETH” and quoteCurrency as
-“BTC”
-*/
+
 const getExchangeTrades = async (baseCurrency, quoteCurrency) => {
     const url = `https://api.exchange.coinbase.com/products/${baseCurrency}-${quoteCurrency}/trades?limit=1`;
     const response = await axios.get(url);
@@ -28,33 +20,21 @@ const getExchangeTrades = async (baseCurrency, quoteCurrency) => {
     }));
 };
 
-/*
-Create a “getExchangeVolume” function to retrieve the last 24hr volume data of specific
-trading pairs. The function should have input parameter “baseCurrency” and
-“quoteCurrency” and return output format as the example object;
-The baseCurrency and QuoteCurrency input can be capital or lowercase but the output result
-the format should always show base_currency and quote_currency in capital letters.
-The example shows the result from input baseCurrency as “ETH” and quoteCurrency as “BTC”
-*/
 const getExchangeVolume = async (baseCurrency, quoteCurrency) => {
     const url = `https://api.pro.coinbase.com/products/${baseCurrency}-${quoteCurrency}/stats`;
     const response = await axios.get(url);
     const { data } = response;
     
     return {
-        volume: data.volume,
+        base_currency: baseCurrency,
+        quote_currency: quoteCurrency,  
+        base_volume: data.volume,
         open: data.open,
         high: data.high,
         low: data.low,
     };
 };
 
-/*
-Create a “getLastRate” function to retrieve the last rate data of specific trading pairs. The
-function should have input parameters “base currency” and “quoteCurrency” and return
-output as a number;
-The example shows the result from input baseCurrency as “ETH” and quoteCurrency as “BTC”
-*/
 const getLastRate = async (baseCurrency, quoteCurrency) => {
     const url = `https://api.pro.coinbase.com/products/${baseCurrency}-${quoteCurrency}/ticker`;
     const response = await axios.get(url);
@@ -63,18 +43,8 @@ const getLastRate = async (baseCurrency, quoteCurrency) => {
     return data.price;
 };
 
-/*
-Create a “getOrderBook” function to retrieve the last 24hr volume data of specific trading
-pairs. The function should have input parameters “baseCurrency” and “quoteCurrency” and
-return output as the example object;
-Bids should be ordered from highest price to lowest price.
-Asks should be ordered from lowest price to highest price.
-The example shows the result from input baseCurrency as “ETH” and quoteCurrency as “BTC”
-*/
-
-
 const getOrderBook = async (baseCurrency, quoteCurrency) => {
-    const url = `https://api.pro.coinbase.com/products/${baseCurrency}-${quoteCurrency}/book`;
+    const url = `https://api.pro.coinbase.com/products/${baseCurrency}-${quoteCurrency}/book?level=3`;
     const response = await axios.get(url);
     const { data } = response;
     
@@ -83,3 +53,6 @@ const getOrderBook = async (baseCurrency, quoteCurrency) => {
         asks: data.asks,
     };
 };
+
+getOrderBook('ETH','BTC').then((result) =>{ console.log(result)
+});
