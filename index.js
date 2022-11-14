b6315935d10815221f63265baf219256ae039991
maythasit thamronglearkait <maythasit.th@gmail.com>
2022-11-14 07:51:24 +0700

    test

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 
 // will use this one to call all the function 
+const coinbase = require('./coinbase.js')
+
+console.log(coinbase.getExchangeTrades('eth','btc'));
+coinbase.getExchangeVolume('ETH','BTC').then(console.log);
+coinbase.getLastRate('ETH','btc').then(console.log);
+coinbase.getOrderBook('eth','BTC').then(console.log);
 
 
